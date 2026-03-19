fd0f4e40937cf166931306fc1fb9bc01f4e52676
wirelover <ersl@tuta.io>
2026-03-19 22:29:25 +0300

    chore: add vulture whitelist for pytest fixtures

---
+++ vulture_whitelist.py
@@ -0,0 +1,6 @@
+# vulture_whitelist.py
+
+from tests.fixtures.config_fixtures import *
+from tests.fixtures.events_fixtures import *
+from tests.fixtures.uart_fixtures import *
+from tests.fixtures.vision_fixtures import *
